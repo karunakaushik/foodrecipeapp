385d5e0f4f587f4bcbc5af1b2b3fcd90e5664b39
karunakaushik <karunakaushik22800@gmail.com>
2020-12-27 17:00:16 +0530

    Updates

---
+++ precache-manifest.e258d07f5f1aa61e0649aec101803275.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e8be90b794ccd5f770dd46a2548bf8",
+    "url": "/foodrecipeapp/index.html"
+  },
+  {
+    "revision": "3623ed4dfc46b9399003",
+    "url": "/foodrecipeapp/static/css/main.7e792098.chunk.css"
+  },
+  {
+    "revision": "a3a44042ccab6ebf06ff",
+    "url": "/foodrecipeapp/static/js/2.349e742b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodrecipeapp/static/js/2.349e742b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3623ed4dfc46b9399003",
+    "url": "/foodrecipeapp/static/js/main.8cc70ac7.chunk.js"
+  },
+  {
+    "revision": "d354dbe53cc66c53bd75",
+    "url": "/foodrecipeapp/static/js/runtime-main.4f072380.js"
+  },
+  {
+    "revision": "d729597eba2392bd1da55f37d2433fa8",
+    "url": "/foodrecipeapp/static/media/bg.d729597e.jpg"
+  },
+  {
+    "revision": "2d0ff9ee44f424d041421b2c249ecbbd",
+    "url": "/foodrecipeapp/static/media/cooking.2d0ff9ee.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/foodrecipeapp/static/media/heart.a8cd76c1.svg"
+  }
+]);
