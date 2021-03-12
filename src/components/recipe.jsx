e5c3844a2788040b183fda0ddad4589db70064db
karunakaushik <karunakaushik22800@gmail.com>
2021-03-12 11:26:13 +0530

    minor change

--- src/components/recipe.jsx
+++ src/components/recipe.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import style from './src/css/recipe.module.css'
 
-const Recipe = ({title,calories,image,ingredients,url }) => {
+const Recipe = ({ title, calories, image, ingredients, url }) => {
 
-    return(
+    return (
         <div className={style.recipe}>
-                   <h1>Recipe</h1>
-                   <h1>{title}</h1>
-                  <p><b>Calories: {calories}</b></p>
-                <img className={style.image} src={image} alt=""/>
-             <ol>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                    // <li>{ingredient.food}</li>
-                    
+            <h1>Recipe</h1>
+            <h1>{title}</h1>
+            <p><b>Calories: {calories}</b></p>
+            <img className={style.image} src={image} alt="" />
+            <div className={style.discriptonrecipe}>
+                <ol>
+                    {ingredients.map(ingredient => (
+                        <li>{ingredient.text}</li>
+                        // <li>{ingredient.food}</li>
 
-                ))}
-            </ol>
-            <p>Link: <a href={url}>{url}</a></p>     
+
+                    ))}
+                </ol>
+                <p>Link: <a href={url}>{url}</a></p>
+            </div>
         </div>
     );
 
